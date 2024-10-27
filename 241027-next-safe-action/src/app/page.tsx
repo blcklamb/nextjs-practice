fa3d43fa0ba34bac3cde13f27a571efbe11b9b30
blcklamb <whenucan35@gmail.com>
2024-10-27 15:04:06 +0900

    🎉 next-safe-action example

---
+++ 241027-next-safe-action/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="max-w-[600px] h-fit">
+        <div className="text-3xl">My TODOs</div>
+      </div>
+    </div>
+  );
+}
